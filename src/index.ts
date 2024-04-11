e70dd32bbda1a350bbbcba7d24e66cf021405b1d
Nitesh Shetye <niteshshetye204@gmail.com>
2024-04-11 15:35:35 +0530

    error function return

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,59 @@
 import zod from "zod";
 
+export function maxLengthErrorMessage(inputName: string, inputLength: number) {
+  return `${inputName} cannot exceed ${inputLength} characters`;
+}
+
+export function minLengthErrorMessage(inputName: string, inputLength: number) {
+  return `${inputName} must be at least ${inputLength} characters long`;
+}
+
+export function emptyErrorMessage(inputName: string) {
+  return `${inputName} cannot be empty`;
+}
+
+export function notValidErrorMessage(inputName: string) {
+  return `${inputName} is not valid`;
+}
+
 export const signupBody = zod.object({
-  name: zod.string({ invalid_type_error: "Name can't be empty" }),
-  email: zod.string().email("Email is not valid"),
-  password: zod.string().min(6, "Password should be greater than 6 letters"),
+  name: zod
+    .string({ invalid_type_error: emptyErrorMessage("Name") })
+    .min(2, minLengthErrorMessage("Name", 2))
+    .max(100, maxLengthErrorMessage("Name", 100)),
+  email: zod
+    .string()
+    .email(notValidErrorMessage("Email"))
+    .max(150, maxLengthErrorMessage("Email", 150)),
+  password: zod
+    .string()
+    .min(6, minLengthErrorMessage("Password", 6))
+    .max(20, maxLengthErrorMessage("Password", 20)),
 });
 
 export const signinBody = zod.object({
-  email: zod.string().email("Email is not valid"),
-  password: zod.string().min(6, "Password should be greater than 6 letters"),
+  email: zod
+    .string()
+    .email(notValidErrorMessage("Email"))
+    .max(150, maxLengthErrorMessage("Email", 150)),
+  password: zod
+    .string()
+    .min(6, minLengthErrorMessage("Password", 6))
+    .max(20, maxLengthErrorMessage("Password", 20)),
 });
 
 export const createPostBody = zod.object({
-  title: zod.string().min(3, "Title should atleast contain 3 letters"),
-  content: zod.string().min(3, "Content should atleast contain 3 letters"),
+  title: zod.string().min(3, minLengthErrorMessage("Title", 3)),
+  content: zod.string().min(3, minLengthErrorMessage("Content", 3)),
 });
 
 export const updatePostBody = zod.object({
   title: zod
     .string()
-    .min(3, "Title should atleast contain 3 letters")
+    .min(3, minLengthErrorMessage("Title", 3))
+    .max(200)
     .optional(),
-  content: zod
-    .string()
-    .min(3, "Content should atleast contain 3 letters")
-    .optional(),
+  content: zod.string().min(3, minLengthErrorMessage("Content", 3)).optional(),
   published: zod.boolean().optional(),
   deleted: zod.boolean().optional(),
 });
